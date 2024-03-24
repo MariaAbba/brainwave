96150cca8ebfbe4c9e6555a05340aa805aa30faa
Mary <gretchen7887@gmail.com>
2024-03-24 12:08:15 +0400

    Small fix of hero section

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -7,6 +7,7 @@ import { heroIcons } from "../constants";
 import { ScrollParallax } from "react-just-parallax";
 import Generating from "./Generating";
 import Notification from "./Notification";
+import CompanyLogos from "./CompanyLogos";
 
 const Hero = () => {
   null;
@@ -34,7 +35,7 @@ const Hero = () => {
             </span>
           </h1>
           <p className="body-1 max-w-3xl mx-auto mb-6 text-n-2 lg:mb-8">
-            Unleash the power of AI Brainwave. Upgrade your productiviti with
+            Unleash the power of AI Brainwave. Upgrade your productivity with
             Brainwave, the open AI chat app.
           </p>
           <Button href="/pricing" white>
@@ -88,6 +89,8 @@ const Hero = () => {
           </div>
           <BackgroundCircles />
         </div>
+
+        <CompanyLogos className="hidden relative z-10 mt-20 lg:block" />
       </div>
       <BottomLine />
     </Section>
