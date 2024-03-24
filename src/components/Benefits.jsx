4be404cbfb40dc7304f14d63113702962913d3e1
Mary <gretchen7887@gmail.com>
2024-03-24 18:29:25 +0400

    Finish Collaboration section

--- src/components/Benefits.jsx
+++ src/components/Benefits.jsx
@@ -2,9 +2,9 @@ import React from "react";
 import Heading from "./Heading";
 import Section from "./Section";
 import { benefits } from "../constants";
-import Arrow from '../assets/svg/Arrow'
-import {GradientLight} from './design/Benefits'
-import ClipPath from '../assets/svg/ClipPath'
+import Arrow from "../assets/svg/Arrow";
+import { GradientLight } from "./design/Benefits";
+import ClipPath from "../assets/svg/ClipPath";
 
 const Benefits = () => {
   return (
@@ -28,29 +28,35 @@ const Benefits = () => {
                 <h5>{item.title}</h5>
                 <p className="body-2 mb-6 text-n-3">{item.text}</p>
                 <div className="flex items-center mt-auto">
-                  <img 
-                  src={item.iconUrl}
-                  width={48}
-                  height={48}
-                  alt={item.title}
+                  <img
+                    src={item.iconUrl}
+                    width={48}
+                    height={48}
+                    alt={item.title}
                   />
-                  <p className="ml-auto font-code text-xs font-bold text-n-1 uppercase tracking-wider">Explore More</p>
+                  <p className="ml-auto font-code text-xs font-bold text-n-1 uppercase tracking-wider">
+                    Explore More
+                  </p>
                   <Arrow />
                 </div>
               </div>
 
               {item.light && <GradientLight />}
 
-              <div className="absolute inset-0.5 bg-n-8"
-              style={{ clipPath: 'url(#benefits)'}}>
-
-                <div className="absolute opacity-0 inset-0 transition-opacity hover:opacity-10">{item.imageUrl && (
-                  <img 
-                  src={item.imageUrl}
-                  width={380}
-                  height={362}
-                  className="w-full h-full object-cover"/>
-                )}</div>
+              <div
+                className="absolute inset-0.5 bg-n-8"
+                style={{ clipPath: "url(#benefits)" }}
+              >
+                <div className="absolute opacity-0 inset-0 transition-opacity hover:opacity-10">
+                  {item.imageUrl && (
+                    <img
+                      src={item.imageUrl}
+                      width={380}
+                      height={362}
+                      className="w-full h-full object-cover"
+                    />
+                  )}
+                </div>
               </div>
 
               <ClipPath />
