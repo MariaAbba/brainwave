e6db1c80a4168b492416fb103c3708059f04249c
Mary <gretchen7887@gmail.com>
2024-03-26 17:21:48 +0400

    Upd

--- src/components/PricingList.jsx
+++ src/components/PricingList.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import { pricing } from "../constants";
 import Button from "./Button";
+import { check } from "../assets";
 
 const PricingList = () => {
   return (
@@ -8,17 +9,21 @@ const PricingList = () => {
       {pricing.map((item) => (
         <div
           key={item.id}
-          className="w-[19rem] max-lg:w-full h-full px-6 bg-n-8 border-n-6 rounded-[2rem] lg:w-auto even:py-14 odd:py-8"
+          className="w-[19rem] max-lg:w-full h-full px-6 bg-n-8 border border-n-6 rounded-[2rem] lg:w-auto even:py-14 odd:py-8 odd:my-4 [&>h4]:first:text-color-2 [&>h4]:even:text-color-1 [&<h4]:last:text-color-3"
         >
           <h4>{item.title}</h4>
 
-          <p>{item.description}</p>
+          <p className="body-2 min-h-[4rem] mb-3 text-n-1/50">
+            {item.description}
+          </p>
 
-          <div>
+          <div className="flex items-center h-[5.5rem] mb-6">
             {item.price && (
               <>
-                <div>$</div>
-                <div>{item.price}</div>
+                <div className="h3">$</div>
+                <div className="text-[5.5rem] leading-none font-bold">
+                  {item.price}
+                </div>
               </>
             )}
           </div>
@@ -33,9 +38,13 @@ const PricingList = () => {
 
           <ul>
             {item.features.map((feature, index) => (
-<li key={index}>
-  <img src={check} alt="" />
-</li>
+              <li
+                key={index}
+                className="flex items-start py-5 border-t border-n-6"
+              >
+                <img src={check} width={24} height={24} alt="Check" />
+                <p className="body-2 ml-4">{feature}</p>
+              </li>
             ))}
           </ul>
         </div>
