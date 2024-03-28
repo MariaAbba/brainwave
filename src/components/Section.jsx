43f8e6fb79108f793b9731b09986b3968ddb60ec
Mary <gretchen7887@gmail.com>
2024-03-28 21:45:15 +0400

    Upd

--- src/components/Section.jsx
+++ src/components/Section.jsx
@@ -14,7 +14,8 @@ const Section = ({
       className={`
       relative 
       ${
-        customPaddings || `py-10 lg:py-16 xl:py-20 ${crosses ? "lg:py-32" : ""}`
+        customPaddings ||
+        `py-10 lg:py-16 xl:py-20 ${crosses ? "lg:py-32 xl:py-40" : ""}`
       } 
       ${className || ""}`}
     >
